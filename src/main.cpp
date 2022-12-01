8ee661098cf9fe2354698a67761b91debf27777a
Rubén Concejo Martínez <rubenconcejomartinez@gmail.com>
2022-12-01 12:51:09 +0100

    hola

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#define LED_DELAY_MS 500
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(112500);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(LED_DELAY_MS);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(LED_DELAY_MS);
+  Serial.print(LED_DELAY_MS);
+}
